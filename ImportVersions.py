8d177ce46792d0528219ad493d40f7b8429b60c1
Seraphim R.P <srp@kerygma.digital>
2021-05-17 15:58:00 -0400

    Complete the version importer.

--- ImportVersions.py
+++ ImportVersions.py
@@ -1,1 +1,30 @@
-print("sup");
+from pymongo import MongoClient
+from tinydb import TinyDB
+
+client = MongoClient(port=27017)
+db = client.BibleBotBackend
+
+def ImportVersions():
+    mongoversions = db.Versions
+    versions = TinyDB("versiondb")
+
+    for version in versions.all():
+        source = "bg"
+
+        if version["abbv"] in ["BSB", "NHEB", "WBT", "ELXX", "LXX", "NKJV"]:
+            continue
+        elif version["abbv"] in ["KJVA", "FBV"]:
+            source = "ab"
+
+        mongoversions.insert_one({
+            "Name": version["name"],
+            "Abbreviation": version["abbv"],
+            "Source": source,
+            "SupportsOldTestament": version["hasOT"],
+            "SupportsNewTestament": version["hasNT"],
+            "SupportsDeuterocanon": version["hasDEU"]
+        })
+        
+        print("processed version - " + version["name"])
+
+ImportVersions()
